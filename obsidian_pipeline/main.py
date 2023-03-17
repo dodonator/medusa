6f8c0b6d7b2c5fd1b4ced5c8db1b72d300984b3e
dodonator <dodo@chaosdorf.de>
2023-03-17 19:57:44 +0100

    updates main.py / renames convert.py

--- obsidian_pipeline/main.py
+++ obsidian_pipeline/main.py
@@ -1,13 +1,22 @@
-from crawler import Crawler
 import logging
+from pathlib import Path
+
+from obsidian_pipeline.converter import Converter
+from obsidian_pipeline.crawler import Crawler
 
 logging.basicConfig(filename="crawler.log", filemode="w", level=logging.DEBUG)
 
 root = "https://md.chaosdorf.de"
 start = "navigation"
 
-crawler = Crawler(root)
+pad_dir: Path = Path.cwd() / Path("pads")
+vault_dir: Path = Path.cwd() / Path("obsidian_vault")
+
+crawler = Crawler(root, pad_dir)
 pads = crawler.crawl(start)
 
 for pad in pads:
     crawler.download(pad)
+
+converter = Converter(root, pad_dir, vault_dir)
+converter.convert()
