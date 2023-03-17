f05bffa272a6a35e161d8b3d2ca9f6dcec87cd5a
dodonator <dodo@chaosdorf.de>
2023-03-17 18:35:29 +0100

    function for conversion

--- obsidian_pipeline/convert.py
+++ obsidian_pipeline/convert.py
@@ -12,18 +12,26 @@ LINK_PATTERN = (
 )
 LINK_TEMPLATE = "[{}]({}/{})"
 
-pad_directory = Path.cwd() / Path("output")
-pad_files: Generator = pad_directory.glob("*.md")
 
-file: Path
-for file in pad_files:
-    pad_content: str = file.read_text(encoding="UTF-8")
-    m = re.findall(LINK_PATTERN, pad_content)
-    for match in m:
+def convert_to_obsidian_links(pad_path: Path) -> int:
+    pad_content: str = pad_path.read_text(encoding="UTF-8")
+    matches = re.findall(LINK_PATTERN, pad_content)
+
+    for match in matches:
         text, url, pad = match
         # TODO: clean pad from queries
         old: str = LINK_TEMPLATE.format(text, url, pad)
         new: str = f"[[{pad}|{text}]]"
         pad_content = pad_content.replace(old, new)
 
-    file.write_text(pad_content, encoding="UTF-8")
+    pad_path.write_text(pad_content, encoding="UTF-8")
+    return len(matches)
+
+
+pad_directory = Path.cwd() / Path("output")
+pad_files: Generator = pad_directory.glob("*.md")
+
+file: Path
+for file in pad_files:
+    number: int = convert_to_obsidian_links(file)
+    print(f"converted {number} links in {file.name}")
