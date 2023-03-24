aa4920ac9af853f224530db1e25675e085732241
dodonator <dodo@chaosdorf.de>
2023-03-24 18:48:34 +0100

    refactores main file

--- medusa/main.py
+++ medusa/main.py
@@ -7,27 +7,30 @@ from utils import get_title
 
 logging.basicConfig(filename="crawler.log", filemode="w", level=logging.DEBUG)
 
-root = r"https://md.chaosdorf.de"
-start = "navigation"
+ROOT = r"https://md.chaosdorf.de"
+START = "navigation"
 
 pad_dir: Path = Path.cwd() / Path("pads")
 vault_dir: Path = Path.cwd() / Path("obsidian_vault")
 
-crawler = Crawler(root, pad_dir)
-pads = crawler.crawl(start)
+crawler: Crawler = Crawler(ROOT, pad_dir)
+pads: set[str] = crawler.crawl(START)
 
-for pad in pads:
-    crawler.download(pad)
+pad_id: str
+for pad_id in pads:
+    crawler.download(pad_id)
 
-converter = Converter(root, pad_dir, vault_dir)
+converter: Converter = Converter(ROOT, pad_dir, vault_dir)
 converter.convert()
 
 # rename pads in vault
-for pad in vault_dir.glob("*.md"):
-    pad_content = pad.read_text(encoding="UTF-8")
+pad_path: Path
+for pad_path in vault_dir.glob("*.md"):
+    pad_content: str = pad_path.read_text(encoding="UTF-8")
     title: str = get_title(pad_content)
+
     if title:
         new_filename: str = f"{title}.md"
         new_path: Path = vault_dir / Path(new_filename)
         new_path.write_text(pad_content)
-        pad.unlink()
+        pad_path.unlink()
