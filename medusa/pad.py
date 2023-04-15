31ee208ed707d6305aed0f3d44bde53937019a58
dodonator <dodo@chaosdorf.de>
2023-04-15 11:56:08 +0200

    extract is now a pad method

--- medusa/pad.py
+++ medusa/pad.py
@@ -1,8 +1,13 @@
 import logging as log
 from pathlib import Path
+from typing import Generator
 from urllib.parse import urlparse
 
+import pandoc
 import requests
+from pandoc.types import Link, Pandoc
+
+from medusa.link import PadLink
 
 
 class Pad:
@@ -31,3 +36,23 @@ class Pad:
             raise Exception("Couldn't download pad {self.url}")
         self.content = text
         return text
+
+    def extract(self) -> list[PadLink]:
+        doc: Pandoc = pandoc.read(self.get())
+        blocks: list = doc[1]
+
+        # filters all link objects
+        link_objects: Generator = (
+            block for block in pandoc.iter(blocks) if isinstance(block, Link)
+        )
+
+        links: list = []
+        link_obj: Link
+        for link_obj in link_objects:
+            pad_link = PadLink(link_obj)
+
+            if pad_link.root == self.root:
+                log.info(f"found link: {pad_link}")
+                links.append(pad_link)
+
+        return links
