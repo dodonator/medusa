7ed22d99e7f2b560ee71f66e89e7ca59e8788e75
dodonator <dodo@chaosdorf.de>
2023-04-15 14:30:39 +0200

    removes mistyped import

--- medusa/model.py
+++ medusa/model.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import logging as log
 import re
 from pathlib import Path
-from traceback import extract_stack
 from typing import Generator
 from urllib.parse import ParseResult, urljoin, urlparse
 
