555e8a6556ebbc38ef0fd4249bbf77c158c1b22a
dodonator <dodo@chaosdorf.de>
2023-04-15 11:36:22 +0200

    constructor now uses urlparse

--- medusa/link.py
+++ medusa/link.py
@@ -1,45 +1,30 @@
-from urllib.parse import urljoin, urlparse
+from urllib.parse import ParseResult, urljoin, urlparse
 
 import pandoc
 from pandoc.types import Link
 
 
-def pad_id_from_url(url: str) -> str:
-    """Returns pad_id from url.
-    Args:
-        url (str): url to the pad
-    Returns:
-        str: pad id
-    """
-    pad_id: str = urlparse(url).path[1:]
-    return pad_id
-
-
-def clean_url(url_str: str) -> str:
-    """
-    Removes queries from url.
-    Args:
-        url_str (str): url as str
-    Returns:
-        str: resulting url
-    """
-    url_without_queries = urljoin(url_str, urlparse(url_str).path)
-    return url_without_queries
-
-
 class PadLink:
     raw: str
-    url: str
+    clean: str
+    root: str
     text: str
     pad_id: str
 
     def __init__(self, link_obj: Link):
         # Link(Attr, [Inline], Target)
         _, inline, target = link_obj
+
         self.raw = pandoc.write(link_obj).strip()
-        self.url = clean_url(target[0])
+        url: ParseResult = urlparse(target[0])
+
+        # remove queries and fragments
+        clean_url = urljoin(url.geturl(), url.path)
+        self.clean = clean_url
+
+        self.root = url.netloc
+        self.pad_id = url.path[1:]
         self.text = pandoc.write(inline).strip()
-        self.pad_id = pad_id_from_url(self.url)
 
     def __repr__(self) -> str:
-        return f"PadLink(text='{self.text}',url='{self.url}')"
+        return f"PadLink(text='{self.text}',url='{self.clean}')"
