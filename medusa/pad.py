257d7615cb3845ceea4ef3f625fe856f25731c10
dodonator <dodo@chaosdorf.de>
2023-04-15 12:13:39 +0200

    get_title is now a pad method

--- medusa/pad.py
+++ medusa/pad.py
@@ -1,17 +1,19 @@
 import logging as log
 from pathlib import Path
+import re
 from typing import Generator
-from urllib.parse import urlparse
+from urllib.parse import ParseResult, urlparse
 
 import pandoc
 import requests
-from pandoc.types import Link, Pandoc
+from pandoc.types import Header, Link, Pandoc
 
 from medusa.link import PadLink
 
 
 class Pad:
     name: str
+    title: str
     url: str
     root: str
     filename: Path
@@ -19,9 +21,11 @@ class Pad:
 
     def __init__(self, url: str) -> None:
         self.url = url
-        self.root = urlparse(url).netloc
+        parse_result: ParseResult = urlparse(url)
+        self.root = parse_result.netloc
+        self.name = parse_result.path
 
-    def get(self) -> str:
+    def get_content(self) -> str:
         if hasattr(self, "content"):
             return self.content
 
@@ -30,15 +34,54 @@ class Pad:
         status_code: int = response.status_code
 
         if status_code == 200:
-            text = response.text
+            content = response.text
         else:
             log.error(f"Couldn't download pad {self.url}")
             raise Exception("Couldn't download pad {self.url}")
-        self.content = text
-        return text
+        self.content = content
+        return content
+
+    def get_title(self) -> str:
+        if hasattr(self, "title"):
+            return self.title
+
+        doc: Pandoc = pandoc.read(self.get_content())
+        meta: list = doc[0]
+        blocks: list = doc[1]
+        title: str
+
+        header: list[Header] = [
+            block for block in blocks if isinstance(block, Header) and block[0] == 1
+        ]
+
+        # tries to get title from meta data
+        meta_dict: dict = meta[0]
+        if "title" in meta_dict:
+            title = pandoc.write(meta_dict.get("title")).strip()
+            log.info(f"found title {title!r} in meta data")
+            return title
+
+        # uses first h1 header as title
+        elif header:
+            title = pandoc.write(header[0]).strip()
+            log.info(f"first header was {title!r}")
+
+        else:
+            log.info("no title found")
+            title = self.name
+
+        # clean title
+        source_pattern = r"[\-~#/ ]+"
+        target_pattern = r"_"
+        clean_title: str = re.sub(source_pattern, target_pattern, title)
+        clean_title = clean_title.strip("_")
+
+        self.title = clean_title
+
+        return clean_title
 
     def extract(self) -> list[PadLink]:
-        doc: Pandoc = pandoc.read(self.get())
+        doc: Pandoc = pandoc.read(self.get_content())
         blocks: list = doc[1]
 
         # filters all link objects
--- medusa/utils.py
+++ medusa/utils.py
@@ -1,10 +1,5 @@
 import hashlib
-import logging as log
-import re
 from pathlib import Path
-
-import pandoc
-from pandoc.types import Header, Pandoc
 
 
 def hash_file(path: Path):
@@ -27,42 +22,3 @@ def hash_file(path: Path):
             md5.update(data)
 
     return md5
-
-
-def get_title(pad_content: str) -> str:
-    """Tries to extract title of pad from content.
-
-    Args:
-        pad_content (str): content of pad
-
-    Returns:
-        str: pad title
-    """
-    doc: Pandoc = pandoc.read(pad_content)
-    meta: list = doc[0]
-    meta_dict: dict = meta[0]
-    title: str
-
-    # tries to get title from meta data
-    if "title" in meta_dict:
-        title = pandoc.write(meta_dict.get("title")).strip()
-        log.info(f"found title {title!r} in meta data")
-        return title
-
-    blocks: list = doc[1]
-    header: list[Header] = [
-        block for block in blocks if isinstance(block, Header) and block[0] == 1
-    ]
-    if header:
-        # uses first h1 header as title
-        title = pandoc.write(header[0]).strip()
-        log.info(f"first header was {title!r}")
-
-        source_pattern = r"[\-~#/ ]+"
-        target_pattern = r"_"
-        clean_title: str = re.sub(source_pattern, target_pattern, title)
-        clean_title = clean_title.strip("_")
-        return clean_title
-
-    log.info("no title found")
-    return ""
