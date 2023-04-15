29e61f59a6eb110c71adad9a6406a865cd615be4
dodonator <dodo@chaosdorf.de>
2023-04-15 12:42:28 +0200

    combines link and pad to model

--- medusa/model.py
+++ medusa/model.py
@@ -1,14 +1,46 @@
+from __future__ import annotations
 import logging as log
 from pathlib import Path
 import re
 from typing import Generator
-from urllib.parse import ParseResult, urlparse
+from urllib.parse import ParseResult, urljoin, urlparse
 
 import pandoc
 import requests
 from pandoc.types import Header, Link, Pandoc
 
-from medusa.link import PadLink
+
+class PadLink:
+    raw: str
+    clean: str
+    root: str
+    text: str
+    _pad: Pad
+
+    def __init__(self, link_obj: Link):
+        # Link(Attr, [Inline], Target)
+        _, inline, target = link_obj
+
+        self.raw = pandoc.write(link_obj).strip()
+        url: ParseResult = urlparse(target[0])
+
+        # remove queries and fragments
+        clean_url = urljoin(url.geturl(), url.path)
+        self.clean = clean_url
+
+        self.root = url.netloc
+        self.pad_id = url.path[1:]
+        self.text = pandoc.write(inline).strip()
+
+    @property
+    def pad(self):
+        if hasattr(self, "_pad"):
+            return self._pad
+        self._pad = Pad(self.clean)
+        return self._pad
+
+    def __repr__(self) -> str:
+        return f"PadLink(text='{self.text}',url='{self.clean}')"
 
 
 class Pad:
