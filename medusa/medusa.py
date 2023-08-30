c833c640ef0c1430ae540c0ba774c231851f0312
Dominik Stempel <dominik.stempel@sogeti.com>
2023-08-30 11:50:19 +0200

    docstrings, lazy formatting, custom Exception

--- medusa/medusa.py
+++ medusa/medusa.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+"""
+medusa - Get a local copy of your HedgeDoc, inside an obsidian vault.
+
+Step 1: Download HedgeDoc Pad.
+Step 2: Crawl the pad for all linked pads (links with same domain).
+Step 3: Repeat Step 1 and 2 for each found pad.
+Step 4: Rename filename to pad title (optional).
+"""
 import logging as log
 from argparse import ArgumentParser, BooleanOptionalAction
 from pathlib import Path
@@ -9,7 +17,14 @@ import pandoc
 import requests
 from pandoc.types import Link, Pandoc
 
-log.basicConfig(filename=f"{Path(__file__).stem}.log", filemode="w", level=log.INFO)
+
+class DownloadError(Exception):
+    pass
+
+
+log.basicConfig(
+    filename=f"{Path(__file__).stem}.log", filemode="w", level=log.INFO
+)
 
 
 def download(url: str) -> str:
@@ -25,14 +40,14 @@ def download(url: str) -> str:
         str: pad content
     """
     url = f"{url}/download"
-    response: requests.models.Response = requests.get(url)
+    response: requests.models.Response = requests.get(url, timeout=5)
     status_code: int = response.status_code
 
     if status_code == 200:
         content: str = response.text
     else:
-        log.error(f"Couldn't download pad {url}")
-        raise Exception("Couldn't download pad {url}")
+        log.error("Couldn't download pad %s", url)
+        raise DownloadError(f"Couldn't download pad {url}")
     return content
 
 
@@ -56,19 +71,35 @@ def extract_link_objects(pad_content: str) -> list[Link]:
 
 
 def get_intern_links(pad_content: str) -> list[ParseResult]:
+    """Reads all intern links from pad content.
+
+    Args:
+        pad_content (str): pad content
+
+    Returns:
+        list[ParseResult]: list of urls
+    """
     links: list[ParseResult] = []
     link_obj: Link
     for link_obj in extract_link_objects(pad_content):
         target = link_obj[2]
         url: ParseResult = urlparse(target[0])
         if url.netloc != DOMAIN:
-            log.info(f"{url} is an external url")
+            log.info("%s is an external url", url)
             continue
         links.append(url)
     return links
 
 
 def substitute_links(pad_content: str) -> str:
+    """Substitutes markdown link syntax with obsidian link syntax.
+
+    Args:
+        pad_content (str): current pad content
+
+    Returns:
+        str: substituted content
+    """
     doc: Pandoc = pandoc.read(pad_content)
     blocks: list = doc[1]
 
@@ -91,17 +122,36 @@ def substitute_links(pad_content: str) -> str:
         html_link: str = f"[{text}]({url_str})"
         obsidian_link: str = f"[[{pad_id}.md|{text}]]"
 
-        log.info(f"replaced {html_link!r} with {obsidian_link!r}")
+        log.info(
+            "replaced %s with %s",
+            repr(html_link),
+            repr(obsidian_link),
+        )
 
         pad_content = pad_content.replace(html_link, obsidian_link)
     return pad_content
 
 
 def clean_url(url: ParseResult) -> str:
+    """Cleans url.
+
+    Removes Fragments and leaves only the path.
+
+    Args:
+        url (ParseResult): url to clean
+
+    Returns:
+        str: cleaned url
+    """
     return urljoin(url.geturl(), url.path)
 
 
 def rename_links(working_dir: Path):
+    """Renames file to markdown title.
+
+    Args:
+        working_dir (Path): path to vault
+    """
     translation_table: dict[Path, str] = {}
 
     file: Path
@@ -122,32 +172,37 @@ def rename_links(working_dir: Path):
     for file, title in translation_table.items():
         content: str = file.read_text()
 
-        for f, t in translation_table.items():
-            content = content.replace(f.name, t)
+        for from_link, to_link in translation_table.items():
+            content = content.replace(from_link.name, to_link)
 
         # save changed content
         file.write_text(content)
 
         # rename current file
-        log.info(f"renamed {file.name} to {title}")
+        log.info("renamed %s to %s", file.name, title)
         file.rename(working_dir / Path(f"{title}"))
 
 
-def crawl(start: str) -> None:
+def crawl(root_url: str) -> None:
+    """Start crawling for pad links.
+
+    Args:
+        root_url (str): start url
+    """
     to_check: set[str] = set()
     checked: set[str] = set()
 
-    to_check.add(start)
+    to_check.add(root_url)
 
     while to_check:
         current_url: str = to_check.pop()
         current_pad: str = download(current_url)
         pad_id: str = urlparse(current_url).path.strip("/")
 
-        log.info(f"{pad_id=}")
+        log.info("pad_id: %s", pad_id)
 
         links: list[ParseResult] = get_intern_links(current_pad)
-        log.info(f"{len(links)} link objects were found")
+        log.info("%i link objects were found", len(links))
 
         print(f"found {len(links):>3} links in {pad_id!r}")
 
@@ -158,7 +213,7 @@ def crawl(start: str) -> None:
         for link in cleaned_links:
             if link == current_url:
                 # ignore circular links
-                log.info(f"ignored {link} (circular)")
+                log.info("ignored %s (circular)", link)
                 continue
 
             to_check.add(link)
@@ -167,7 +222,7 @@ def crawl(start: str) -> None:
         filename: str = f"{pad_id}.md"
         filepath: Path = WORKING_DIR / Path(filename)
 
-        log.info(f"save pad @ {filepath}")
+        log.info("save pad @ %s", filepath)
 
         # substitute html link with obsidian syntax
         substituted_pad = substitute_links(current_pad)
@@ -187,7 +242,9 @@ if __name__ == "__main__":
 
     # add arguments
     parser.add_argument("start_url", type=str, help="start url")
-    parser.add_argument("-p", "--path", type=Path, required=False, help="vault path")
+    parser.add_argument(
+        "-p", "--path", type=Path, required=False, help="vault path"
+    )
     parser.add_argument(
         "-r",
         "--rename",
